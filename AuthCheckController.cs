e3afdda50493e51d503087d27c881f3195d22544
Jim <jgett@hotmail.com>
2014-07-25 18:09:48 -0400

    the sample is more correct now

--- AuthCheckController.cs
+++ AuthCheckController.cs
@@ -2,7 +2,7 @@ namespace Sample.Controllers
 {
     public class AuthCheckController : Controller
     {
-		public ActionResult AuthCheck(string cookieValue = "")
+        public ActionResult AuthCheck(string cookieValue = "")
         {
             Client c = null;
 
@@ -12,20 +12,22 @@ namespace Sample.Controllers
 
                 if (!string.IsNullOrEmpty(ticket.Name))
                 {
-					//use whatever data access method you have here...
+                    //use whatever data access method you have here...
                     c = DA.Search<Client>(x => x.UserName == ticket.Name).FirstOrDefault();
                     if (c == null)
-                        return Json(new { success = false, message = string.Format("no client found for '{0}'", ticket.Name) });
+                        return Json(new { success = false, message = string.Format("no client found for '{0}'", ticket.Name), authenticated = false, username = "" });
 
-					//in my case a Client object has the method Roles which returns a string array. If the client is staff then one
-					//of the elements will be the string "staff", but you can create this array in whatever way makes sense
-						
+                    //in my case a Client object has the method Roles which returns a string array. If the client is staff then one
+                    //of the elements will be the string "staff", but you can create this array in whatever way makes sense
+
                     //success!
                     return Json(new { success = true, message = "", authenticated = !ticket.Expired, username = ticket.Name, roles = c.Roles(), lastName = c.LName, firstName = c.FName, email = c.PrimaryEmail(), expiration = ticket.Expiration, expired = ticket.Expired });
-				}
+                }
 
                 return Json(new { success = false, message = "no username found", authenticated = false, username = "" });
             }
-			else
-				return Json(new { success = false, message = "missing cookieValue", authenticated = false , username = "" });
+            else
+                return Json(new { success = false, message = "missing cookieValue", authenticated = false, username = "" });
         }
+    }
+}
